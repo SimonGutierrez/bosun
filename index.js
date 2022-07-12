cc508b154fddf6f2e40519c4cac2008b9b799d2b
Manuel Bugallo <marvinody@gmail.com>
2022-07-11 20:24:31 -0400

    fixes dumb issue with eas and entryPoint

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ import { registerRootComponent } from "expo";
 import * as React from "react";
 import { Provider } from "react-redux";
 
-import App from "./App";
-import store from "./store";
+import App from "./src/App";
+import store from "./src/store";
 
 const app = () => (
   <Provider store={store}>
